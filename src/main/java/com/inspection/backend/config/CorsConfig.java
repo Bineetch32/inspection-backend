405b4cd4e5cb75d6f3bb09408ff65a16f25f62a9
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-20 22:41:07 +0530

    Allow Angular localhost development ports in CORS

--- src/main/java/com/inspection/backend/config/CorsConfig.java
+++ src/main/java/com/inspection/backend/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOriginPatterns("http://localhost:[*]")
                         .allowedMethods(
                                 "GET",
                                 "POST",
